ecd57fb7b0edd63130084f37d5e9bcf966695bbc
Dara_code <114925970+Daracode07@users.noreply.github.com>
2023-11-15 15:17:31 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+ScrollReveal({ 
+    reset: true,
+    duration: 2000,
+    delay: 200,
+    distance: '80px'
+});
+ ScrollReveal().reveal('.about',{origin: 'top'}); 
+ ScrollReveal().reveal('.service',{origin: 'bottom', delay: 200});        
+ ScrollReveal().reveal('.testimonials',{origin: 'right', delay: 300, duration: 1000});      
+ ScrollReveal().reveal('.testimonials-text',{origin: 'top', delay: 300, duration: 1000}) 
+ ScrollReveal().reveal('.team',{origin: 'right', delay: 300, duration: 1000});     
+ ScrollReveal().reveal('.expertise',{origin: 'left', delay:400, durartion: 200, distance: '100px'});        
+
+
+ 
